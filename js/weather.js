5e40e2611ea82ec7718c10ca74843832882b9800
wututua <wututua@qq.com>
2022-06-22 22:19:14 +0800

    Site updated: 2022-06-22 22:18:50

---
+++ js/weather.js
@@ -0,0 +1,10 @@
+WIDGET = {
+  "CONFIG": {
+    "layout": "2",
+    "width": 230,
+    "height": 270,
+    "background": "1",
+    "dataColor": "FFFFFF",
+    "key": "c6f481fb384c4fbab8101646cf33a3d4"
+  }
+}
